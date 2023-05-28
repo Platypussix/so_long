e66edc75cded038ceeab284576652cf033018ccf
Platyx <armand.mediouna@gmail.com>
2023-05-28 20:59:03 +0200

    commit maison

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "mlx/mlx.h"
+
+int main(int ac, char **av)
+{
+	void *mlx_ptr;
+	void *win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, av[1]);
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+
+}
