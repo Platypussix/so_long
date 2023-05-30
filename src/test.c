a3aff474e838d7821288dc29311f1b5c934ddcf9
Armand MEDIOUNA <amedioun@e2r11p12.clusters.42paris.fr>
2023-05-30 14:30:03 +0200

    commit auto de la flemme absolue

--- src/test.c
+++ src/test.c
@@ -1,11 +1,15 @@
+#include "../so_long.h"
 
 int main(int ac, char **av)
 {
 	void *mlx_ptr;
 	void *win_ptr;
 
+	(void) ac;
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, av[1]);
+	while (1)
+		;
 	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
