b045563ed03d31c8a438de869b8de71426bce454
Platyx <armand.mediouna@gmail.com>
2023-06-28 10:39:15 +0200

    commit maison

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: amedioun <amedioun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 10:40:47 by amedioun          #+#    #+#             */
-/*   Updated: 2023/05/29 10:45:43 by amedioun         ###   ########.fr       */
+/*   Updated: 2023/06/28 10:27:33 by amedioun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,30 @@
 # include "mlx/mlx_int.h"
 # include "libft/libft.h"
 
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
 
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+#define GREEN_PIXEL 0x00FF00
+#define BLUE_PIXEL 0x0000FF
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}	t_data;
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bbp;
+	int		line_len;
+	int		endian;
+}	t_img;
 
 #endif
